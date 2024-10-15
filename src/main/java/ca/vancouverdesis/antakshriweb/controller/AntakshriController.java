3545032e954ab9b4fde80d79c002f25f43117359
amitkumar <amkumar@sierrawireless.com>
2024-10-14 17:02:30 -0700

    added logs

--- src/main/java/ca/vancouverdesis/antakshriweb/controller/AntakshriController.java
+++ src/main/java/ca/vancouverdesis/antakshriweb/controller/AntakshriController.java
@@ -3,6 +3,8 @@ package ca.vancouverdesis.antakshriweb.controller;
 import ca.vancouverdesis.antakshriweb.domain.Buzzer;
 import ca.vancouverdesis.antakshriweb.domain.Scores;
 import ca.vancouverdesis.antakshriweb.service.AntakshriService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,20 +14,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
-
 @Controller
 public class AntakshriController {
+    Logger logger = LoggerFactory.getLogger(AntakshriController.class);
     @Autowired
     AntakshriService antakshriService;
 
     @GetMapping("/buzzers")
     @ResponseBody
     public Map<String, Map<Integer, Buzzer>> getBuzzers() {
+        logger.info("Getting buzzers");
         return antakshriService.getBuzzers();
     }
 
     @GetMapping("/home")
     public String home(Model model) {
+        logger.info("Home page");
         model.addAttribute("message", "Register your team");
         model.addAttribute("buzzer", new Buzzer());
         return "add";
@@ -33,6 +37,7 @@ public class AntakshriController {
 
     @PostMapping("/register")
     public String register(Model model, Buzzer buzzer) {
+        logger.info("Registering team : "+ buzzer.getTeamName() + " : " + buzzer.getPersonName());
         model.addAttribute("buzzer", buzzer);
         model.addAttribute("message", "Welcome " + buzzer.getPersonName() + "!");
         return "buzzer";
@@ -40,12 +45,14 @@ public class AntakshriController {
 
     @GetMapping("/buzzer")
     public String buzzer(Model model, Buzzer buzzer) {
+        logger.info("Buzzer page");
         model.addAttribute("message", "Welcome " + buzzer.getPersonName() + "!");
         return "buzzer";
     }
 
     @PostMapping("/clicked")
     public String clicked(Model model, @RequestBody Buzzer buzzer) {
+        logger.info("Buzzer clicked : "+ buzzer.getTeamName() + " : " + buzzer.getPersonName());
         model.addAttribute("message", "Welcome " + buzzer.getPersonName() + "!");
         antakshriService.addBuzzer(buzzer);
         return "buzzer";
@@ -53,17 +60,20 @@ public class AntakshriController {
 
     @GetMapping("/reset")
     public void reset(Model model) {
+        logger.info("Resetting buzzers");
         antakshriService.resetBuzzer();
     }
 
     @GetMapping("/antakshri")
     public String antakshri(Model model) {
+        logger.info("Antakshri page");
         model.addAttribute("message", "Happy Diwali");
         return "antakshri";
     }
 
     @GetMapping("/admin")
     public String admin(Model model) {
+        logger.info("Admin page");
         model.addAttribute("message", "Admin");
         model.addAttribute("scores", new Scores());
         return "admin";
@@ -71,6 +81,7 @@ public class AntakshriController {
 
     @PostMapping("/submit-scores")
     public String submitScores(@RequestBody Scores scores) {
+        logger.info("Submitting scores: " + scores.getTeamName() + " : " + scores.getScore());
         antakshriService.submitScores(scores);
         return "admin";
     }
@@ -78,6 +89,9 @@ public class AntakshriController {
     @GetMapping("/scores")
     @ResponseBody
     public Map<String, Integer> getScores() {
-        return antakshriService.getScores();
+        Map<String, Integer> scores = antakshriService.getScores();
+        logger.info("Getting scores: entry set" + scores.entrySet() + "!");
+        logger.info("Getting scores: key set" + scores.keySet() + "!");
+        return scores;
     }
 }
--- src/main/java/ca/vancouverdesis/antakshriweb/service/AntakshriService.java
+++ src/main/java/ca/vancouverdesis/antakshriweb/service/AntakshriService.java
@@ -3,6 +3,8 @@ package ca.vancouverdesis.antakshriweb.service;
 import ca.vancouverdesis.antakshriweb.domain.Buzzer;
 import ca.vancouverdesis.antakshriweb.domain.Scores;
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
@@ -13,6 +15,7 @@ import java.util.concurrent.ConcurrentSkipListMap;
 
 @Service
 public class AntakshriService {
+    Logger logger = LoggerFactory.getLogger(AntakshriService.class);
     private ConcurrentSkipListMap<ZonedDateTime, Buzzer> audienceMap;
     private ConcurrentSkipListMap<ZonedDateTime, Buzzer> playerMap;
     @Getter
@@ -35,6 +38,7 @@ public class AntakshriService {
     }
 
     public void addBuzzer(Buzzer buzzer) {
+        logger.info("Adding buzzer : " + buzzer.getTeamName() + " : " + buzzer.getPersonName());
         if("AUDIENCE".equalsIgnoreCase(buzzer.getTeamName())) {
             audienceMap.put(ZonedDateTime.now(), buzzer);
         } else {
@@ -43,11 +47,13 @@ public class AntakshriService {
     }
 
     public void resetBuzzer() {
+        logger.info("Service: Resetting buzzers");
         audienceMap.clear();
         playerMap.clear();
     }
 
     public Map<String, Map<Integer, Buzzer>> getBuzzers() {
+        logger.info("Service: Getting buzzers");
         Map<String, Map<Integer, Buzzer>> returnMap = new HashMap<>();
         Map<Integer, Buzzer> buzzerMap = new HashMap<>();
         Map<Integer, Buzzer> finalBuzzerMap = buzzerMap;
@@ -66,6 +72,7 @@ public class AntakshriService {
     }
 
     public void submitScores(Scores teamScore) {
+        logger.info("Submitting scores : " + teamScore.getTeamName() + " : " + teamScore.getScore());
         scores.put(teamScore.getTeamName(), teamScore.getScore());
     }
 }
